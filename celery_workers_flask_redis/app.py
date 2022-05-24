dc11b49ff8d9fec820e2096928af09abba6ee987
ezzeddin <ezzeddinabdullah@gmail.com>
2022-05-24 11:35:32 +0200

    Add Celery workers tutorial with Flask and Redis

---
+++ celery_workers_flask_redis/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from celery import Celery
+
+app = Flask(__name__)
+app.config["CELERY_BROKER_URL"] = "redis://localhost:6379"
+
+celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
+celery.conf.update(app.config)
+
+
+@celery.task()
+def add(x, y):
+    return x + y
+
+
+@app.route('/')
+def add_task():
+    for i in range(10):
+        add.delay(i, i)
+    return jsonify({'status': 'ok'})
